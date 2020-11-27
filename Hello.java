a58be78b0490e6e9cadb2e0c59dcd60e1d61d905
qhrms97 <qhrms97@gmail.com>
2020-11-27 23:20:08 +0900

    c7cd3

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
    public static void main(String[] args) {
       System.out.println("Hello, world from GIT!");
-      System.out.println("this is f301b commit!");
+      System.out.println("this is c7cd3 commit!");
    }
 }
